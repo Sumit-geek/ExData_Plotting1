613cc0f5cad8ba6f14d82842647eb7ebb047d41c
Sumit <ernarangsumit@gmail.com>
2017-08-28 23:32:57 +1000

    assigment-1

---
+++ plot2.R
@@ -0,0 +1,9 @@
+file <- "./data/household_power_consumption.txt"
+hpc <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+febDaysData <- hpc[hpc$Date %in% c("1/2/2007","2/2/2007") ,]
+
+datetime <- strptime(paste(febDaysData$Date, febDaysData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+febDaysData$Global_active_power <- as.numeric(febDaysData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, febDaysData$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
