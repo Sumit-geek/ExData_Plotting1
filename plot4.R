c011b6508fd6e0a10f8fcb154cbd39c3d0fbdc81
Sumit <ernarangsumit@gmail.com>
2017-08-30 15:11:04 +1000

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -24,6 +24,6 @@ lines(datetime, febDaysData$Sub_metering_2, type="l", col="red")
 lines(datetime, febDaysData$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
 
-plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+plot(datetime, febDaysData$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 
 dev.off()
